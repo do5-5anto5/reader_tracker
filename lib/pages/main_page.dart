f80a8aabf9838a24e0f676caac5496fbef638272
Jonathan <dos.santos.2k23@gmail.com>
2025-05-22 22:32:13 -0300

    Feature: Create Home, Saved and Favorites screens and attach them to BottomNavigationBar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:reader_tracker/pages/saved_screen.dart';
+
+import 'favorites_screen.dart';
+import 'home_screen.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,6 +14,12 @@ class MainPage extends StatefulWidget {
 class _MainPage extends State<MainPage> {
   int _currentIndex = 0;
 
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const SavedScreen(),
+    const FavoriteScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,15 +27,18 @@ class _MainPage extends State<MainPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: (const Text('A.Reader')),
       ),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           const BottomNavigationBarItem(icon: Icon(Icons.save), label: 'Saved'),
-          const BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites',),
+          const BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites',
+          ),
         ],
         selectedItemColor: Theme.of(context).colorScheme.onPrimary,
-        unselectedItemColor: Theme.of(context).colorScheme.onPrimary,
+        unselectedItemColor: Theme.of(context).colorScheme.onSurfaceVariant,
         onTap: (value) {
           setState(() {
             _currentIndex = value;
