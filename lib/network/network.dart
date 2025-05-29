a52df39236e8de574e4a325e7c858ea45b609c62
Jonathan <dos.santos.2k23@gmail.com>
2025-05-29 19:16:46 -0300

    Feature: Implement json decoding from network.dart

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/book.dart';
@@ -9,18 +7,23 @@ import '../models/book.dart';
 class NetWork {
   static const String baseUrl = 'https://www.googleapis.com/books/v1/volumes';
 
-  // Future<List<Book>> searchBooks(String query) async {
-  Future<void> searchBooks(String query) async {
+  Future<List<Book>> searchBooks(String query) async {
     var url = Uri.parse('$baseUrl?q=$query');
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
      // we have data (json)
      var data = json.decode(response.body);
-     if (kDebugMode) {
-       print(data['totalItems']);
+     if (data['items'] != null && data['items'] is List) {
+       List<Book> books = (data['items'] as List<dynamic>)
+       .map((book) => Book.fromJson(book as Map<String, dynamic>))
+       .toList();
+
+       return books;
+     } else {
+       return [];
      }
     } else {
-      // return [];
+      throw(Exception('Failed to load books'));
     }
 }}
