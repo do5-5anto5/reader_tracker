93c657c28c6f8bb2ae9a917f9b598e06efa73754
Jonathan <dos.santos.2k23@gmail.com>
2025-05-31 16:57:25 -0300

    Feature: Add image and titles to DetailsScreen

--- lib/pages/book_details.dart
+++ lib/pages/book_details.dart
@@ -13,12 +13,57 @@ class BookDetailsScreen extends StatefulWidget {
 class _BookDetailsScreenState extends State<BookDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    final bookDetailsArguments = ModalRoute.of(context)?.settings.arguments as BookDetailsArguments;
+    final bookDetailsArguments =
+        ModalRoute.of(context)?.settings.arguments as BookDetailsArguments;
     final Book book = bookDetailsArguments.itemBook;
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(book.title),
-        // title: const Text('Book Details'),
+      appBar: AppBar(title: Text(book.title)),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                if (book.imageLinks.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Image.network(
+                      book.imageLinks['thumbnail'] ?? '',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                Column(
+                  children: [
+                    Text(
+                      book.title,
+                      style: textTheme.headlineSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                    if (book.title == '' || book.authors.isEmpty)
+                      Text(
+                        book.authors.join(', '),
+                        style: textTheme.titleSmall,
+                      ),
+                    Text(
+                      'Published ${book.publishedDate}',
+                      style: textTheme.bodySmall,
+                    ),
+                    Text(
+                      'Page count ${book.pageCount.toString()}',
+                      style: textTheme.bodySmall,
+                    ),
+                    Text(
+                      'Language: ${book.language}',
+                      style: textTheme.bodySmall,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
