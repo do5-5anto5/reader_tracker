e90a1815b664172a3bb76ed7327e21d3a7f16dfc
Jonathan <dos.santos.2k23@gmail.com>
2025-06-01 09:36:58 -0300

    Feature: Read saved books i saved_screen.dart

--- lib/pages/saved_screen.dart
+++ lib/pages/saved_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:reader_tracker/db/database_helper.dart';
+
+import '../models/book.dart';
 
 class SavedScreen extends StatefulWidget {
   const SavedScreen({super.key});
@@ -11,8 +14,25 @@ class _SavedScreenState extends State<SavedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Saved'),
+      appBar: AppBar(title: const Text('Saved')),
+      body: Padding(
+        padding: EdgeInsets.all(12),
+        child: FutureBuilder<List<Book>>(
+          future: DatabaseHelper.instance.readAllBooks(),
+          builder:
+              (context, snapshot) =>
+                  snapshot.hasData
+                      ? ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        var book = snapshot.data![index];
+                        return ListTile(
+                          title: Text(book.title),
+                        );
+
+                  })
+                      : const Center(child: CircularProgressIndicator()),
+        ),
       ),
     );
   }
