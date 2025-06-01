420ddcafa6a0fad35eb529a4234283d730a05024
Jonathan <dos.santos.2k23@gmail.com>
2025-06-01 09:56:20 -0300

    Feature: Improve saved books viewing by wrapping it in a card, add favorite and delete button views too

--- lib/pages/saved_screen.dart
+++ lib/pages/saved_screen.dart
@@ -16,21 +16,38 @@ class _SavedScreenState extends State<SavedScreen> {
     return Scaffold(
       appBar: AppBar(title: const Text('Saved')),
       body: Padding(
-        padding: EdgeInsets.all(12),
+        padding: const EdgeInsets.all(12),
         child: FutureBuilder<List<Book>>(
           future: DatabaseHelper.instance.readAllBooks(),
           builder:
               (context, snapshot) =>
                   snapshot.hasData
                       ? ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index) {
-                        var book = snapshot.data![index];
-                        return ListTile(
-                          title: Text(book.title),
-                        );
-
-                  })
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          var book = snapshot.data![index];
+                          return Card(
+                            child: ListTile(
+                              title: Text(book.title),
+                              leading: Image.network(
+                                book.imageLinks['smallThumbnail'] ?? '',
+                                fit: BoxFit.cover,
+                              ),
+                              trailing: Icon(Icons.delete),
+                              subtitle: Column(
+                                children: [
+                                  Text(book.authors.join(', ')),
+                                  ElevatedButton.icon(onPressed: () async {
+                                    
+                                  },
+                                  icon: Icon(Icons.favorite),
+                                  label: const Text('Add to Favorites'))
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      )
                       : const Center(child: CircularProgressIndicator()),
         ),
       ),
